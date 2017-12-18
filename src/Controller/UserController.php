61526b2869276fa54cb8992c99a38d28fdc3f736
tralgar <leo.poupet@gmail.com>
2017-12-18 19:32:38 +0100

    save before custom actions

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -4,48 +4,18 @@ namespace App\Controller;
 
 use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
 class UserController extends Controller
 {
-    public function list()
+
+    public function showUserAction(Request $request)
     {
-        $users = $this->getDoctrine()
-            ->getRepository(User::class)
-            ->findAll();
+        $id = $request->query->get('id');
 
-        foreach ($users as $user) {
-            $user->url = $this->generateUrl('user_show', ['id' => $user->getId()]);
-        }
-
-        return $this->render('user/list.html.twig', ['users' => $users]);
-    }
-
-    public function show(int $id)
-    {
-        $user = $this->getDoctrine()
-            ->getRepository(User::class)
-            ->find($id);
-
-        if (!$user) {
-            throw $this->createNotFoundException('Aucun utilisateur trouvé pour l\'id ' . $id);
-        }
-
-        return $this->render('user/show.html.twig', ['user' => $user]);
-    }
-
-    public function new()
-    {
-        $em = $this->getDoctrine()->getManager();
-
-        $user = new User();
-        $user->setUsername("Tralgar");
-        $user->setEmail("leo.poupet@gmail.com");
-        $user->setPassword("test");
-        $user->setIsActive(true);
-
-        $em->persist($user);
-        $em->flush();
-
-        return $this->render('user/new.html.twig');
+        return $this->redirectToRoute('easyadmin', array(
+            'action' => 'show',
+            'id' => $id,
+        ));
     }
 }
