be0c1ff86487401a3ba361c3030b4c2f6f56eece
Léo <leo.poupet@gmail.com>
2018-01-21 22:10:56 +0100

    updates

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -6,5 +6,14 @@ use JavierEguiluz\Bundle\EasyAdminBundle\Controller\AdminController as BaseAdmin
 
 class AdminController extends BaseAdminBundle
 {
+  protected function updateEntity($entity)
+  {
+    exit;
+    // UserPasswordEncoderInterface $encoder
+    // $user = $this->getUser();
+    // $encoded = $encoder->encodePassword($user, $plainPassword);
+
+    // $user->setPassword($encoded);
+  }
 
 }
