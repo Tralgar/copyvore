758e997dce537f842e9e01a2a07cacc42c5b8913
Léo <leo.poupet@gmail.com>
2018-01-22 09:25:18 +0100

    fixing css bugs

--- public/login/js/main.js
+++ public/login/js/main.js
@@ -72,14 +72,14 @@
     $('.btn-show-pass').on('click', function(){
         if(showPass == 0) {
             $(this).next('input').attr('type','text');
-            $(this).find('i').removeClass('zmdi-eye');
-            $(this).find('i').addClass('zmdi-eye-off');
+            $(this).find('i').removeClass('fa-eye');
+            $(this).find('i').addClass('fa-eye-slash');
             showPass = 1;
         }
         else {
             $(this).next('input').attr('type','password');
-            $(this).find('i').addClass('zmdi-eye');
-            $(this).find('i').removeClass('zmdi-eye-off');
+            $(this).find('i').addClass('fa-eye');
+            $(this).find('i').removeClass('fa-eye-slash');
             showPass = 0;
         }
         
